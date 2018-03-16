271601c838f61fbc36b92a35115889c7617b911e
Andrew Oliver <oliveraj@lopers.unk.edu>
2018-03-15 19:22:39 -0500

    Added some nonsense.

--- 1.0.0/install.sh
+++ 1.0.0/install.sh
@@ -3,8 +3,11 @@
 #Default runtime variable if none is supplied
 HELLO_WORLD_STATEMENT=${HELLO_WORLD_STATEMENT:='You are lazy for using the defaults, you blue-footed booby.'}
 
-echo "You said: "
-echo $HELLO_WORLD_STATEMENT
+PATH="$PATH:/usr/games"
+export PATH
+
+fortune | cowsay -f dragon
+cowsay $HELLO_WORLD_STATEMENT
 
 #Self-destruct
 rm -- "$0" && exit 0
